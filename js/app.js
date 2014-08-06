939e073a39eeb644d0631a95e38315ab4afe65b0
Matt Brophy <matt@brophy.org>
2014-08-05 20:35:51 -0400

    Added barcode scanner usage

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var UCE = window.UCE || {};
+var UCE = window.UCE || {},
+    Handlebars = window.Handlebars;
 
 // From: http://bit.ly/1gAKKPP
 function isInPhoneGap() {
@@ -25,6 +26,7 @@ UCE.init = function () {
 
 UCE.bindListeners = function () {
   $('.btn-scan').on('click', UCE.scanTicket);
+  $('.btn-submit').on('click', UCE.submitManualCode);
   $('.hide').on('click', UCE.reset);
 };
 
@@ -60,27 +62,105 @@ UCE.reset = function () {
 };
 
 UCE.scanTicket = function () {
-  var dfd = new $.Deferred();
+  var scanner = UCE.getbarcodeScanner();
 
-  function success(code) {
-    code = '123456789';
-    UCE.log('Scanned code: ' + code);
-    UCE.showValid();
-    dfd.resolve();
+  function success(result) {
+    if (result.cancelled !== 0) {
+      UCE.log('User cancelled the scan.');
+      return;
+    } else if (result.format !== 'QR_CODE') {
+      UCE.log('QR code not found.');
+      return;
+    }
+
+    UCE.log('Scanned code: ' + result.text);
+    UCE.submitTicket(result.text);
   }
 
   function error () {
     UCE.showInvalid();
-    dfd.reject();
   }
 
-  if (Math.random() > 0.5) {
-    setTimeout(success, 250);
-  } else {
-    setTimeout(error, 250);
+  if (!scanner) {
+    success({
+      cancelled: 0,
+      format: 'QR_CODE',
+      text: window.prompt('Enter a code', '123456789')
+    });
   }
 
-  return dfd.promise();
+  scanner.scan(success, error);
+};
+
+UCE.getbarcodeScanner = function () {
+  if (window.cordova && window.cordova.plugins &&
+      window.cordova.plugins.barcodeScanner) {
+    return window.cordova.plugins.barcodeScanner;
+  }
+  return null;
+};
+
+UCE.submitManualCode = function (e) {
+  var $code = $('.input-qrcode'),
+      code = $code.val();
+
+  if (code.trim() === '') {
+    UCE.log('Invalid code entered');
+    window.alert('Please enter a valid Ticket Code');
+    return;
+  }
+
+  UCE.submitTicket(code);
+};
+
+UCE.submitTicket = function (code) {
+  var dfd = new $.Deferred();
+
+  setTimeout(function () {
+    var mockData;
+
+    if (Math.random() < 0.1) {
+      return dfd.reject();  // Mimic failed ajax request
+    } else if (Math.random() < 0.55) {
+      mockData = {
+        valid: true,
+        ticketCode: code,
+        ticketType: 'VIP',
+        attendeeName: 'John Doe'
+      };
+    } else {
+      mockData = {
+        valid: false,
+        ticketCode: code,
+        error: 'Ticket already redeemed'
+      };
+    }
+
+    dfd.resolve(mockData);
+  }, 1000);
+
+  function success(data) {
+    var source, template;
+    if (data.valid) {
+      source = $('#tpl-valid').html();
+      template = Handlebars.compile(source);
+      $('.page-valid .content').html(template(data));
+      return UCE.showValid();
+    } else {
+      source = $('#tpl-invalid').html();
+      template = Handlebars.compile(source);
+      $('.page-invalid .content').html(template(data));
+      return UCE.showInvalid();
+    }
+  }
+
+  function error(e) {
+    UCE.log('Could not submit ticket');
+    window.alert('Ticket could not be submitted.  Please make sure you have ' +
+                 'a valid internet connection and try again.');
+  }
+
+  return dfd.promise().then(success, error);
 };
 
 UCE.deviceReadyDfd.promise().done(UCE.init);
