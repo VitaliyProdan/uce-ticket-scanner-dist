f597789bdc7fd08b8ddace364c3e528404d0cf2b
Matt Brophy <matt@brophy.org>
2014-08-05 21:56:17 -0400

    Added login functionality and page transitions

--- js/app.js
+++ js/app.js
@@ -19,12 +19,26 @@ UCE.log = function (s) {
   console.log('UCE: ' + s);
 };
 
+UCE.cancelEvent = function (e) {
+  if (!e) { return false; }
+  e.preventDefault();
+  e.stopPropagation();
+  return false;
+};
+
 UCE.init = function () {
   UCE.log('App ready!');
   UCE.bindListeners();
+
+  if (UCE.isLoggedIn()) {
+    UCE.showScan();
+  } else {
+    UCE.showLogin();
+  }
 };
 
 UCE.bindListeners = function () {
+  $('.btn-login').on('click', UCE.submitLogin);
   $('.btn-scan').on('click', UCE.scanTicket);
   $('.btn-submit').on('click', UCE.submitManualCode);
   $('.hide').on('click', UCE.reset);
@@ -34,35 +48,172 @@ UCE.showPage = function (selector) {
   var $el = $(selector),
       dfd = new $.Deferred();
 
-  $el.one('transitionend', dfd.resolve);
+  UCE.log('Showing page ' + selector);
   $el.addClass('show');
 
-  return dfd.promise();
+  requestAnimationFrame(function () {
+    UCE.log('Frame 1 ');
+    requestAnimationFrame(function () {
+      UCE.log('Frame 2 - adding fadein ');
+      if (!$el.hasClass('fadein')) {
+        $el.one('transitionend', dfd.resolve);
+        $el.addClass('fadein');
+      } else {
+        dfd.resolve();
+      }
+    });
+  });
+
+  return dfd.promise().then(function () { UCE.log('Done animation'); });
 };
 
 UCE.hidePage = function (selector) {
   var $el = $(selector),
       dfd = new $.Deferred();
 
-  $el.one('transitionend', dfd.resolve);
-  $el.removeClass('show');
+  UCE.log('Hiding page ' + selector);
 
-  return dfd.promise();
+  if ($el.hasClass('fadein')) {
+    $el.one('transitionend', dfd.resolve);
+    $el.removeClass('fadein');
+  } else {
+    dfd.resolve();
+  }
+
+  return dfd.promise().then(function () {
+    UCE.log('Fade done');
+    requestAnimationFrame(function () {
+      UCE.log('Removing show class');
+      $el.removeClass('show');
+    });
+  });
 };
 
 UCE.showValid = _.partial(UCE.showPage, '.page-valid');
 UCE.showInvalid = _.partial(UCE.showPage, '.page-invalid');
+UCE.showLogin = _.partial(UCE.showPage, '.page-login');
+UCE.showScan = _.partial(UCE.showPage, '.page-scan');
 UCE.hideValid = _.partial(UCE.hidePage, '.page-valid');
 UCE.hideInvalid = _.partial(UCE.hidePage, '.page-invalid');
+UCE.hideLogin = _.partial(UCE.hidePage, '.page-login');
+UCE.hideScan = _.partial(UCE.hidePage, '.page-scan');
 
-UCE.reset = function () {
+UCE.loginAjax = function (username, password) {
+  var dfd = new $.Deferred();
+
+  setTimeout(function () {
+    var mockData, rand = Math.random();
+
+    if (rand < 0.1) {
+      return dfd.reject();  // Mimic failed ajax request
+    } else if (rand < 0.4) {
+      mockData = {
+        response: {
+          status: '-1',
+          ClientName: 'John Doe',
+          LogoURL: 'img/uce.jpg'
+        }
+      };
+    } else if (rand < 0.7) {
+      mockData = {
+        response: {
+          status: '0',
+          ClientName: 'John Doe',
+          LogoURL: 'img/uce.jpg'
+        }
+      };
+    } else {
+      mockData = {
+        response: {
+          status: '1',
+          ClientName: 'John Doe',
+          LogoURL: 'img/uce.jpg'
+        }
+      };
+    }
+
+    dfd.resolve(mockData);
+  }, 1000);
+
+  return dfd.promise();
+};
+
+UCE.isLoggedIn = function () {
+  var clientName = window.lscache.get('ClientName');
+  return clientName != null;
+};
+
+UCE.cacheLogin = function (response) {
+  UCE.clearLogin();
+  window.lscache.set('ClientName', response.ClientName, 30);
+  window.lscache.set('LogoURL', response.LogoURL, 30);
+};
+
+UCE.clearLogin = function (response) {
+  window.lscache.remove('ClientName');
+  window.lscache.remove('LogoURL');
+};
+
+UCE.submitLogin = function (e) {
+
+  var username, password;
+
+  UCE.cancelEvent(e);
+
+  function success(response) {
+    if (response.valid) {
+      UCE.cacheLogin(response);
+      return UCE.hideLogin().then(UCE.showScan);
+    } else {
+      UCE.clearLogin(response);
+      window.alert(response.message);
+    }
+  }
+
+  function error(e) {
+    UCE.log('Could not login');
+    window.alert('Login could not be processed.  Please make sure you have ' +
+                 'a valid internet connection and try again.');
+  }
+
+  function enhanceData(data) {
+    if (data.response.status === '-1') {
+      data.response.valid = false;
+      data.response.message = 'Sorry, this account has been locked out.';
+    } else if (data.response.status === '0') {
+      data.response.valid = false;
+      data.response.message = 'Incorrect username/password combination.';
+    } else {
+      data.response.valid = true;
+    }
+    return data.response;
+  }
+
+  username = $('#username').val();
+  password = $('#password').val();
+
+  if (username.length === 0 || password.length === 0) {
+    window.alert('Please enter a username and password');
+    return;
+  }
+
+  return UCE.loginAjax(username, password)
+            .then(enhanceData)
+            .done(success)
+            .fail(error);
+};
+
+UCE.reset = function (e) {
+  UCE.cancelEvent(e);
   UCE.hideValid();
   UCE.hideInvalid();
   $('.input-qrcode').val('');
 };
 
-UCE.scanTicket = function () {
-  var scanner = UCE.getbarcodeScanner();
+UCE.scanTicket = function (e) {
+  var scanner = UCE.getBarcodeScanner();
+
+  UCE.cancelEvent(e);
 
   function success(result) {
     if (result.cancelled !== 0) {
@@ -87,12 +238,13 @@ UCE.scanTicket = function () {
       format: 'QR_CODE',
       text: window.prompt('Enter a code', '123456789')
     });
+    return;
   }
 
   scanner.scan(success, error);
 };
 
-UCE.getbarcodeScanner = function () {
+UCE.getBarcodeScanner = function () {
   if (window.cordova && window.cordova.plugins &&
       window.cordova.plugins.barcodeScanner) {
     return window.cordova.plugins.barcodeScanner;
@@ -110,10 +262,11 @@ UCE.submitManualCode = function (e) {
     return;
   }
 
+  UCE.cancelEvent(e);
   UCE.submitTicket(code);
 };
 
-UCE.submitTicket = function (code) {
+UCE.ticketAjax = function (code) {
   var dfd = new $.Deferred();
 
   setTimeout(function () {
@@ -123,33 +276,40 @@ UCE.submitTicket = function (code) {
       return dfd.reject();  // Mimic failed ajax request
     } else if (Math.random() < 0.55) {
       mockData = {
-        valid: true,
-        ticketCode: code,
-        ticketType: 'VIP',
-        attendeeName: 'John Doe'
+        response: {
+          status: '1',
+          TicketType: 'VIP'
+        }
       };
     } else {
       mockData = {
-        valid: false,
-        ticketCode: code,
-        error: 'Ticket already redeemed'
+        response: {
+          status: '0',
+          TicketType: null
+        }
       };
     }
 
     dfd.resolve(mockData);
   }, 1000);
 
-  function success(data) {
+  UCE.log('Faking ajax call..');
+  return dfd.promise();
+};
+
+UCE.submitTicket = function (code) {
+
+  function success(response) {
     var source, template;
-    if (data.valid) {
+    if (response.valid) {
       source = $('#tpl-valid').html();
       template = Handlebars.compile(source);
-      $('.page-valid .content').html(template(data));
+      $('.page-valid .content').html(template(response));
       return UCE.showValid();
     } else {
       source = $('#tpl-invalid').html();
       template = Handlebars.compile(source);
-      $('.page-invalid .content').html(template(data));
+      $('.page-invalid .content').html(template(response));
       return UCE.showInvalid();
     }
   }
@@ -160,7 +320,16 @@ UCE.submitTicket = function (code) {
                  'a valid internet connection and try again.');
   }
 
-  return dfd.promise().then(success, error);
+  function enhanceData(data) {
+    data.response.valid = (data.response.status === '1');
+    data.response.TicketCode = code;
+    return data.response;
+  }
+
+  return UCE.ticketAjax(code)
+            .then(enhanceData)
+            .done(success)
+            .fail(error);
 };
 
 UCE.deviceReadyDfd.promise().done(UCE.init);
